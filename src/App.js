fb169158ff30fd723fa709510d15ebb3298ddffa
BrendaC111 <brenda.correa@urp.edu.pe>
2024-05-11 23:32:15 -0500

    primer commit -b

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hola</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
